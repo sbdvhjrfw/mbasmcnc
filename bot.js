c16c207239a653cbc888c203e2107f5548390179
sbdvhjrfw <33731226+sbdvhjrfw@users.noreply.github.com>
2018-08-14 16:08:55 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,8 +6,8 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
+    if (message.content != 'smgnskdjgnkjdfnkf') {
+    	message.reply('Én csak egy bot vagyok, hiába írsz üzenetet.');
   	}
 });
 
